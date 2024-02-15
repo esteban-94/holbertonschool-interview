c0bd5fcb78da4935622da9f40f9b03ef4a9ae017
Esteban <juan.esteban.94@outlook.com>
2024-02-15 11:28:58 -0500

    fix docu

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -13,13 +13,10 @@ patron_log = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\s+-\s+\[\d{4}-\d{2}-\d{2}\s+\
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 i = 0
 counter = 0
-"""
-Script that reads stdin line by line and computes metrics.
-After every 10 lines and/or a keyboard interruption (CTRL + C),
-print these statistics from the beginning: Total file size
-and number of lines by status code.
-excecute: ./0-generator.py | ./0-stats.py
-"""
+
+def print_metrics():
+    """Method to print the statistics from the beginning"""
+
 while True:
     entrada = sys.stdin.readline()
     if re.match(patron_log, entrada):
